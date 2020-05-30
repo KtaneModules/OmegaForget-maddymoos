34eee5cf7113959f0fbfaa8e127c494260c953d2
Cooldoom5 <Cooldoorn5@gmail.com>
2020-05-29 22:29:44 -0400

    Solve animation+Manual
    poggers

--- Forget/Assets/forget.cs
+++ Forget/Assets/forget.cs
@@ -105,13 +105,14 @@ public class forget : MonoBehaviour {
             maxStage = Bomb.GetSolvableModuleNames().Where(a => !ignoredModules.Contains(a)).Count();
 		else
 	 //I WOULD LIKE TO MODIFY THE STAGE COUNT IN THE TESTHARNESS
-			maxStage = 10;
+			maxStage = 1;
 		if(maxStage == 0){
 			Module.HandlePass();
 			ColorChanger[0].material = Lights[4];
 			ColorChanger[1].material = Lights[4];
 			Lightarray[0].color = Colors[4];
 			Lightarray[1].color = Colors[4];
+			solved = true;
 		}
 		else{
 		PStorage = new string[maxStage];
@@ -364,6 +365,7 @@ public class forget : MonoBehaviour {
 		}
 	}
 	IEnumerator SolveAnimation(){
+		StartCoroutine(SolveLights());
 			Audio.PlaySoundAtTransform("Module_Solved", Numbers[0].transform);
 		yield return new WaitForSeconds(19f);
 		float tom=0;while(tom<=.5f){
@@ -386,6 +388,7 @@ public class forget : MonoBehaviour {
 			BCChanger[i].material = BColours[3]; 
 			}
 			Module.HandlePass();
+			solved = true;
 	}
 	IEnumerator FinalCheck(){
 		Audio.PlaySoundAtTransform("Reveal_"+Rnd.Range(1,4), Numbers[0].transform);
@@ -636,7 +639,89 @@ public class forget : MonoBehaviour {
             //once it reaches here, we know it's modulated and we can return it
             return num;
         }
-    }
+	}
+		IEnumerator SolveLights(){
+			/*1 long 4 short 1 long 3 short*/
+			Lightarray[0].color = Colors[13];
+			ColorChanger[0].material = Lights[13];
+			Lightarray[1].color = Colors[0];
+			ColorChanger[1].material = Lights[0];
+			yield return new WaitForSeconds(1.2f);
+			
+			for(int i=1;i<13;i++){
+			Lightarray[0].color = Colors[13-i];
+			ColorChanger[0].material = Lights[13-i];
+			Lightarray[1].color = Colors[i];
+			ColorChanger[1].material = Lights[i];
+			yield return new WaitForSeconds(.3f);
+			}
+			
+			ColorChanger[0].material = Lights[1];
+			Lightarray[1].color = Colors[13];
+			Lightarray[0].color = Colors[1];
+			ColorChanger[1].material = Lights[13];
+			yield return new WaitForSeconds(1.2f);
+			
+			for(int i=0;i<4;i++){
+			Lightarray[0].color = Colors[13-i];
+			ColorChanger[0].material = Lights[13-i];
+			Lightarray[1].color = Colors[i];
+			ColorChanger[1].material = Lights[i];
+			yield return new WaitForSeconds(.3f);
+			}
+			
+			ColorChanger[0].material = Lights[10];
+			Lightarray[0].color = Colors[10];
+			Lightarray[1].color = Colors[4];
+			ColorChanger[1].material = Lights[4];
+			yield return new WaitForSeconds(1.2f);
+			
+			for(int i=5;i<9;i++){
+			Lightarray[0].color = Colors[13-i];
+			ColorChanger[0].material = Lights[13-i];
+			Lightarray[1].color = Colors[i];
+			ColorChanger[1].material = Lights[i];
+			yield return new WaitForSeconds(.3f);
+			}
+			ColorChanger[0].material = Lights[5];
+			Lightarray[0].color = Colors[5];
+			Lightarray[1].color = Colors[9];
+			ColorChanger[1].material = Lights[9];
+			yield return new WaitForSeconds(1.2f);
+			
+			for(int i=6;i<14;i++){
+			Lightarray[0].color = Colors[13-i];
+			ColorChanger[0].material = Lights[13-i];
+			Lightarray[1].color = Colors[i];
+			ColorChanger[1].material = Lights[i];
+			yield return new WaitForSeconds(.3f);
+			}
+			for(int i=0;i<5;i++){
+			Lightarray[0].color = Colors[13-i];
+			ColorChanger[0].material = Lights[13-i];
+			Lightarray[1].color = Colors[i];
+			ColorChanger[1].material = Lights[i];
+			yield return new WaitForSeconds(.3f);
+			}
+			ColorChanger[0].material = Lights[9];
+			Lightarray[0].color = Colors[9];
+			Lightarray[1].color = Colors[5];
+			ColorChanger[1].material = Lights[5];
+			yield return new WaitForSeconds(1.2f);
+			
+			for(int i=6;i<11;i++){
+			Lightarray[0].color = Colors[13-i];
+			ColorChanger[0].material = Lights[13-i];
+			Lightarray[1].color = Colors[i];
+			ColorChanger[1].material = Lights[i];
+			yield return new WaitForSeconds(.3f);
+			}
+			ColorChanger[0].material = Lights[3];
+			Lightarray[0].color = Colors[3];
+			Lightarray[1].color = Colors[11];
+			ColorChanger[1].material = Lights[11];
+			yield return new WaitForSeconds(1.2f);
+		}
 	#pragma warning disable 414
     private readonly string TwitchHelpMessage = @"!{0} press X# (Waits for the color X to be in the #th position, then presses it) ||Commands can be chained using X# X#...||";
 	#pragma warning restore 414
